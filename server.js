b90d80772d8bed1ff7efd868de610c0c96ef6022
Favour Oghenekowho <gh.oghene@gmail.com>
2022-12-22 21:07:43 +0100

    implemented error handling

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!⚠️ Shutting down...!');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -22,7 +28,15 @@ mongoose
   });
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION!⚠️ Shutting down...!');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
