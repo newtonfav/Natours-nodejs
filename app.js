5d34a18e11ccab6e5b6c40453b10b01902e8bb73
Favour Oghenekowho <gh.oghene@gmail.com>
2022-12-30 22:02:39 +0100

    pug

--- app.js
+++ app.js
@@ -64,7 +64,22 @@ app.use(
 
 //ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Favour',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
