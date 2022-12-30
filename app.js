472dfd087876c9eaf9217206fa57524848adde78
Favour Oghenekowho <gh.oghene@gmail.com>
2022-12-30 14:34:52 +0100

    rendered pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -14,7 +15,13 @@ const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //GLOBAL MIDDLEWARE
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Set security for header
 app.use(helmet());
 
@@ -55,10 +62,11 @@ app.use(
   })
 );
 
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 //ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/review', reviewRouter);
