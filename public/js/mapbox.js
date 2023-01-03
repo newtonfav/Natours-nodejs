539037c53aa7121fe262a622b9b1c386727e92db
Favour Oghenekowho <gh.oghene@gmail.com>
2023-01-03 21:09:25 +0100

    Popup bug resolved

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,45 +1,33 @@
 /* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.location);
+console.log(locations);
 
 var map = L.map('map').setView([51.505, -0.09], 13);
-// const map = L.map('map');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-// L.marker([34.11175, -118.113455]).addTo(map).bindPopup('Location.').openPopup();
-
-// bounds = L.bounds();
-
 const coords = [];
-let layers;
+let markers;
 
 locations.forEach((loc) => {
-  //Add maker
-  // const el = document.createElement('div');
-  // el.className = 'marker';
-
   const [lng, lat] = loc.coordinates;
-
   const temp = [lat, lng];
 
+  L.marker(temp)
+    .bindPopup(
+      `<p style="font-size:10px;">Day ${loc.day}: ${loc.description}</p>`,
+      {
+        autoClose: false,
+      }
+    )
+    .addTo(map)
+    .openPopup();
+
   coords.push(temp);
-
-  const layer = L.marker(temp).addTo(map);
 });
-
-map.eachLayer(function (layer) {
-  layer.bindPopup('Hello').openPopup();
-  console.log('newton');
-});
-// console.log(coords);
 
 map.flyToBounds(coords, { padding: [100, 100] });
-
-// L.marker(loc.coordinates)
-//     .addTo(map)
-//     .bindPopup(`Day ${loc.day}: ${loc.coordinates}`)
-//     .openPopup();
