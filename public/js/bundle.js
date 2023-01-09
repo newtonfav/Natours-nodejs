525529f19d9689620c971720e2aa8354811718d6
Favour Oghenekowho <gh.oghene@gmail.com>
2023-01-09 23:07:05 +0100

    integrated welcome email for new signup

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12359,7 +12359,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51525" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49176" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,61 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+// eslint-disable-next-line node/no-extraneous-require
+const { htmlToText } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //1. Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    //Activate in gmail "less secure app" option when using gmail as a transporter
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Favour Oghenekowho <${process.env.SENDER_EMAIL}>`;
+  }
 
-  //2. Define the email options
-  const mailOptions = {
-    from: `Favour Oghenekowho <${process.env.SENDER_EMAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //sendgrid
+      return 0;
+    }
 
-  //3. Actually send the email
-  await transporter.sendMail(mailOptions);
+    //1. Create a transporter
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      //Activate in gmail "less secure app" option when using gmail as a transporter
+    });
+  }
+
+  //Send Actual Email
+  async send(template, subject) {
+    //1. Render HTML based on a pug template
+    // const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    //2. Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    //3. Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the natours family!');
+  }
 };
-
-module.exports = sendEmail;
